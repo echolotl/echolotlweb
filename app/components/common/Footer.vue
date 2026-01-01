5663e243f47f06ecf287c77dea9114fe95bc67a6
echolotl <69995058+echolotl@users.noreply.github.com>
2026-01-01 00:00:02 -0600

    update copyright and add license information to footer

--- app/components/common/Footer.vue
+++ app/components/common/Footer.vue
@@ -4,7 +4,30 @@
             <div class="content-container">
                 <div class="small-username"></div>
                 <SocialIcons style="image-rendering: pixelated" />
-                <div>© echolotl 2025</div>
+                <div>© 2026 echolotl</div>
+                <div class="license">
+                    <div class="license-image"></div>
+                    <div class="license-text">
+                        Unless otherwise noted, all content on this site is
+                        licensed under the
+                        <a
+                            href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+                            target="_blank"
+                            class="link"
+                            rel="noopener noreferrer"
+                            >CC BY-NC-SA 4.0 International License</a
+                        >.
+                        <br />
+                        Source code is licensed under the
+                        <a
+                            href="https://github.com/echolotl/echolotlweb/blob/master/LICENSE"
+                            target="_blank"
+                            class="link"
+                            rel="noopener noreferrer"
+                            >MIT License</a
+                        >.
+                    </div>
+                </div>
             </div>
         </div>
         <div class="footer-images">
@@ -100,14 +123,16 @@ footer {
     background: linear-gradient(to top, var(--surface), transparent 100%);
     height: 50%;
     width: 100%;
+    padding-bottom: 1.5rem;
 
     .content-container {
         height: 100%;
         max-width: 1200px;
         margin: 0 auto;
+
         display: flex;
         align-items: center;
-        justify-content: center;
+        justify-content: flex-end;
         flex-direction: column;
         gap: 0.25rem;
         filter: drop-shadow(0 1px 0 var(--background))
@@ -128,6 +153,25 @@ footer {
             mask-position: center;
             background-color: var(--text);
             transform: translateX(3px);
+        }
+        .license-image {
+            width: 75px;
+            height: 20px;
+            mask-image: url("/images/cc-by-nc-sa.png");
+            mask-size: contain;
+            mask-repeat: no-repeat;
+            mask-position: center;
+            background-color: var(--text-secondary);
+        }
+        .license-text {
+            font-size: 0.75rem;
+            color: var(--text-secondary);
+        }
+        .license {
+            margin-top: 0.25rem;
+            display: flex;
+            align-items: center;
+            flex-direction: row;
         }
     }
 }
