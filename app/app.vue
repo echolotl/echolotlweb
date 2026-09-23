fe31b44f9875cb66994c96e630982245a5d90882
echolotl <echolotl@echolotl.lol>
2026-09-23 17:17:59 -0500

    umami

--- app/app.vue
+++ app/app.vue
@@ -15,4 +15,14 @@ const { fetchUser } = useAuth();
 onMounted(() => {
   fetchUser();
 });
+
+useHead({
+  script: [
+    {
+      src: "https://umami.echolotl.lol/script.js",
+      "data-website-id": "77e4ea44-c716-4370-822b-c56b37406967",
+      defer: true,
+    },
+  ],
+});
 </script>
